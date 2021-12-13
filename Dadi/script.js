99b6b866b1c6845175c69f358992e907bb0b51b3
chiaraxs <chiaracardinale92@gmail.com>
2021-12-13 17:29:49 +0100

    alert added (dadi)

--- Dadi/script.js
+++ Dadi/script.js
@@ -3,6 +3,7 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+const outputHtml = document.querySelector('.result');
 
 // 1. CREO VARIABILE CON MATH.FLOOR (NR. RANDOM A CUI ASSEGNO VALORE MAX 6) PER TURNO DADO GIOCATORE E LA STAMPO IN CONSOLE.
 let humanNumber = Math.floor(Math.random() * 6); 
@@ -15,16 +16,19 @@ console.log(computerNumber);
 // 3. CREO CONDIZIONE IF PER DECRETARE IL VINCITORE IN BASE AL TIRO DEL DADO: 
 if (humanNumber > computerNumber) {
     // SE IL DADO DEL GIOCATORE > DADO PC -> HAI VINTO!
-    console.log('Hai vinto!');  
+    console.log('Hai vinto!'); 
+    alert(outputHtml.innerHTML = 'Hai vinto!');
 }
 
 else if (humanNumber < computerNumber) {
     // SE IL DADO DEL GIOCATORE < DADO PC -> OPS, HAI PERSO!
     console.log('Ops, hai perso! Ritenta!');
+    alert(outputHtml.innerHTML = 'Ops, hai perso! Ritenta!');
 }   
 
 else {
     // SE DADO DEL GIOCATORE = DADO PC -> HAI PAREGGIATO, RITENTA!
     console.log('Hai pareggiato! Ritenta per vincere!')
+    alert(outputHtml.innerHTML = 'Hai pareggiato! Ritenta per vincere!');
 }
 
